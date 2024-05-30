ea1e6ab79d33b3f2ecc18bdef6cfee7f5388ae57
m521 <mohamedismail.mezhoud@esprit.tn>
2024-05-30 12:04:22 +0100

    SendingMotivationalEmails

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import smtplib
+import datetime as dt
+import random
 
-# Create a connection to the SMTP server
 my_email = "ismailmezhoud2@gmail.com"
 password = "kcoa lvch krjd dpvy"
 
-with smtplib.SMTP('smtp.gmail.com', 587) as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="mohamedismail.mezhoud@esprit.tn",
-        msg="Subject:Hello\n\nThis is my email ."
-    )
+now = dt.datetime.now()  # current date and time
+day_of_week = now.weekday()  # current day of the week
+
+if day_of_week == 1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+        with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs="mohamedismail.mezhoud@esprit.tn",
+                msg=f"Subject:Hello\n\n {quote} ."
+            )
+
+
+
+
 

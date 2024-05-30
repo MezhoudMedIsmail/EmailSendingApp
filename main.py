c80841f8485ea79e63a63f81088215b967e023a7
m521 <mohamedismail.mezhoud@esprit.tn>
2024-05-30 12:19:15 +0100

    SendingMotivationalEmailsV1

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ if day_of_week == 1:
     with open("quotes.txt") as quote_file:
         all_quotes = quote_file.readlines()
         quote = random.choice(all_quotes)
-        with smtplib.SMTP('smtp.gmail.com', 587) as connection:
-            connection.starttls()
-            connection.login(user=my_email, password=password)
-            connection.sendmail(
-                from_addr=my_email,
-                to_addrs="mohamedismail.mezhoud@esprit.tn",
-                msg=f"Subject:Hello\n\n {quote} ."
-            )
+    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="mohamedismail.mezhoud@esprit.tn",
+            msg=f"Subject:Hello\n\n {quote} ."
+        )
 
 
 
